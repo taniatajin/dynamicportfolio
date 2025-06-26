caf0d6bf6c5a516691a6161ea0f81308cf085be8
taniatajin <tania15-4692@diu.edu.bd>
2025-06-26 16:43:01 +0600

    //

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,17 +1,28 @@
 @extends('layout')
 
 @section('content')
-<h1>Contact Me</h1>
-<form>
-    <label>Name:</label><br>
-    <input type="text" name="name"><br>
+<div class="contact-container">
+    <h1 class="contact-title"> Get in Touch</h1>
+    <p class="contact-subtitle">Have a project in mind, a question, or just want to say hello? I’d love to hear from you!</p>
 
-    <label>Email:</label><br>
-    <input type="email" name="email"><br>
+    <form class="contact-form" action="#" method="POST">
+        @csrf
+        <div class="form-group">
+            <label for="name"> Name</label>
+            <input type="text" id="name" name="name" placeholder="Enter your full name" required>
+        </div>
 
-    <label>Message:</label><br>
-    <textarea name="message"></textarea><br>
+        <div class="form-group">
+            <label for="email"> Email</label>
+            <input type="email" id="email" name="email" placeholder="Enter your email address" required>
+        </div>
 
-    <button type="submit">Send</button>
-</form>
+        <div class="form-group">
+            <label for="message"> Message</label>
+            <textarea id="message" name="message" rows="5" placeholder="Write your message here..." required></textarea>
+        </div>
+
+        <button type="submit" class="send-button">🚀 Send Message</button>
+    </form>
+</div>
 @endsection
